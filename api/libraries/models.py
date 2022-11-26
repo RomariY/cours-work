1ed9f41d6841aadde9d5998f4c734a9c3bc96db0
roman.ya <roman.ya@gowombat.team>
2022-11-26 18:01:19 +0200

    Add column to library table

--- api/libraries/models.py
+++ api/libraries/models.py
@@ -1,17 +1,20 @@
 import peewee
+from playhouse.migrate import migrate
 
 from api.base import database
+from api.base.database import migrator
 from api.base.models import PeeweeGetterDict
 from api.base.models import UUIDModel
 
 TYPE_CHOICES = (
-    (0, 'Draft'),
-    (1, 'Published'),
-    (2, 'Deleted')
+    (0, 'Group1'),
+    (1, 'Group2'),
+    (2, 'Group3')
 )
 
 
 class Library(UUIDModel):
+    name = peewee.CharField(max_length=50, null=True)
     type = peewee.CharField(max_length=32, choices=TYPE_CHOICES)
     description = peewee.CharField(max_length=200)
     repo_link = peewee.CharField(max_length=50)
--- api/libraries/schemas.py
+++ api/libraries/schemas.py
@@ -8,25 +8,28 @@ from pydantic import Field
 
 
 class S(str, Enum):
-    draft = 'Draft'
-    published = 'Published'
-    deleted = "Deleted"
+    group1 = 'Group1'
+    group2 = 'Group2'
+    group3 = "Group3"
 
 
 class LibrarySchema(BaseModel):
     id: UUID = Field(default_factory=uuid.uuid4())
+    name: str
     type: S
     description: str
     repo_link: HttpUrl
 
 
 class LibraryCreateSchema(BaseModel):
+    name: str
     type: S
     description: str
     repo_link: HttpUrl
 
 
 class LibraryUpdateSchema(BaseModel):
+    name: Optional[str]
     type: Optional[S]
     description: Optional[str]
     repo_link: Optional[HttpUrl]
