899d8da9f327f8cd5c0c4b1eb6408e5f58b4d45d
RomarioY <roman.ya@gowombat.team>
2022-12-01 11:23:04 +0200

    Fix launch command

--- settings.py
+++ settings.py
@@ -10,8 +10,14 @@ MEDIA_ROOT = 'media'
 MEDIA_URL = f'{BASE_DIR}/media/'
 
 # DATABASE
-DATABASE = os.getenv("DATABASE")
-PORT = os.getenv("PORT", 5432)
-HOST = os.getenv("HOST", "localhost")
-DATABASE_USER = os.getenv("DATABASE_USER")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+# DATABASE = os.getenv("DATABASE")
+# PORT = os.getenv("PORT", 5432)
+# HOST = os.getenv("HOST", "localhost")
+# DATABASE_USER = os.getenv("DATABASE_USER")
+# DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+
+DATABASE = os.environ.get("DATABASE")
+PORT = os.environ.get("PORT", 5432)
+HOST = os.environ.get("HOST", "localhost")
+DATABASE_USER = os.environ.get("DATABASE_USER")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
